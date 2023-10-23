ba1d395dd7dee9b42f950182d407d1ce90a534da
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-10-24 00:40:06 +0200

    Update components.js

--- Utils/components.js
+++ Utils/components.js
@@ -1,22 +1,31 @@
 /**
  * @typedef {object} Props
  * @prop {string} element
- * */
+ * @prop {string} template
+ *
+ */
 
 /**
  *
  * @param {Props} props
  */
 
+
 export const CreateComponent = (props) => {
-  const { element } = props;
+  const { element , template:templateString} = props;
   if (!element.includes("-")) {
     throw new Error("element must include a hyphen");
   }
+
+  
+const template = document.createElement("template");
+template.innerHTML= templateString
+
   class component extends HTMLElement {
     #inner = this.attachShadow({ mode: "open" });
     constructor() {
       super();
+      this.#inner.appendChild(node);
     }
 
     connectedCallback() {
