a1563a89b34cbc11f3ec48425e7dd63dd8887645
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-10-25 12:31:53 +0200

    add sl-buttons

--- view/app.js
+++ view/app.js
@@ -6,33 +6,32 @@ import { State } from "../model/store.js";
  * @returns {any}
  */
 export const app = (state) => {
-    console.log(state);
-    
-    // Correctly destructure 'tasks' from the 'state' parameter
-    const { tasks } = State;
-    const taskAsArray = Object.values(tasks);
+  console.log(state);
 
-    //const taskAsArray = Array.isArray(tasks) ? tasks : Object.values(tasks);
+  // Correctly destructure 'tasks' from the 'state' parameter
+  const { tasks } = State;
+  const taskAsArray = Object.values(tasks);
 
-  
-    return html` <div>
-      <header>
-        <h1>Todo App</h1>
-        <sl-button variant="primary">Primary</sl-button>
-        <form>
-          <label>
-            <span> New Task</span>
-            <input name="title" />
-          </label>
-          <button type="submit">Add Task</button>
-        </form>
-      </header>
-      <main>
-        <ul>
+  //const taskAsArray = Array.isArray(tasks) ? tasks : Object.values(tasks);
 
-        </ul>
-      </main>
-      <h2>Task(${tasks.length || 0})</h2>
-    </div>`;
-  };
-  
+  return html` <div>
+    <sl-dialog open>
+      <form>
+        <sl-input filled  name="title" label="Task Name" required></sl-input>
+
+        <sl-button>Cancel</sl-button>
+
+        <sl-button variant="primary">Save</sl-button>
+      </form>
+    </sl-dialog>
+
+    <header>
+      <h1>Todo App</h1>
+      <sl-button variant="primary">Add Task</sl-button>
+    </header>
+    <main>
+      <ul></ul>
+    </main>
+    <h2>Task(${tasks.length || 0})</h2>
+  </div>`;
+};
